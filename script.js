6df2ada034e29f8048ce354c6eebf80193c557d1
vipulnagarkar102 <vipulnagarkar102@gmail.com>
2024-12-18 16:55:56 +0530

    air quality section

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ let cityInput = document.getElementById('city_input'),
     api_key = '250d8198dcaf602952d44571e3b4b88b';
 currentWeatherCard = document.querySelectorAll('.weather-left .card')[0];
 fiveDaysForecastCard = document.querySelector('.day-forecast');
+aqiCard = document.querySelectorAll('.highlights .card')[0];
+sunriseCard = document.querySelectorAll('.highlights .card')[1];
+aqiList = ['Good', 'Fair', 'Moderate', 'Poor', 'Very poor'];
 
 
 function getweatherDetails(name, lat, lon, country, state) {
     let FORECAST_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api_key}`,
         WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`,
+        AIR_POLLUTION_API_URL = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${api_key}`,
         days = [
             'Sunday',
             'Monday',
@@ -31,6 +35,55 @@ function getweatherDetails(name, lat, lon, country, state) {
             'Nov',
             'Dec'
         ];
+
+    fetch(AIR_POLLUTION_API_URL).then(res => res.json()).then(data => {
+        let { co, no, no2, o3, so2, pm2_5, pm10, nh3 } = data.list[0].components;
+        aqiCard.innerHTML = `
+                            <div class="card-head">
+                            <p>Air Quality</p>
+                            <p class="air-index aqi-${data.list[0].main.aqi}">${aqiList[data.list[0].main.aqi - 1]}</p>
+                        </div>
+                        <div class="air-indices">
+                            <i class="fa-regular fa-wind fa-3x"></i>
+                            <div class="item">
+                                <p>PM2.5</p>
+                                <h2>${pm2_5}</h2>
+                            </div>
+                            <div class="item">
+                                <p>PM10</p>
+                                <h2>${pm10}</h2>
+                            </div>
+                            <div class="item">
+                                <p>So2</p>
+                                <h2>${so2}</h2>
+                            </div>
+                            <div class="item">
+                                <p>CO</p>
+                                <h2>${co}</h2>
+                            </div>
+                            <div class="item">
+                                <p>NO</p>
+                                <h2>${no}</h2>
+                            </div>
+                            <div class="item">
+                                <p>NO2</p>
+                                <h2>${no2}</h2>
+                            </div>
+                            <div class="item">
+                                <p>NH3</p>
+                                <h2>${nh3}</h2>
+                            </div>
+                            <div class="item">
+                                <p>O3</p>
+                                <h2>${o3}</h2>
+                            </div>
+                        </div>
+`;
+    }).catch(() => {
+        alert(`failed to fetch data`);
+    });
+
+
     fetch(WEATHER_API_URL).then(res => res.json()).then(data => {
         let date = new Date();
         currentWeatherCard.innerHTML = `
@@ -51,6 +104,35 @@ function getweatherDetails(name, lat, lon, country, state) {
                     </div>
                 </div>
                 `;
+        let { sunrise, sunset } = data.says,
+            { timezone } = data,
+            sRiseTime = moment.utc(sunrise, 'X').add(timezone, 'seconds').format('hh:mm A'),
+            sSetTime = moment.utc(sunset, 'X').add(timezone, 'seconds').format('hh:mm A');
+        sunriseCard.innerHTML = `
+            <div class="card-head">
+                            <p>Sunrise & Sunset</p>
+                        </div>
+                        <div class="sunrise-sunset">
+                            <div class="item">
+                                <div class="icon">
+                                    <i class="fa-light fa-sunrise fa-4x"></i>
+                                </div>
+                                <div>
+                                    <p>Sunrise</p>
+                                    <h2>${sRiseTime}</h2>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="icon">
+                                    <i class="fa-light fa-sunrise fa-4x"></i>
+                                </div>
+                                <div>
+                                    <p>Sunset</p>
+                                    <h2>${sSetTime}</h2>
+                                </div>
+                            </div>
+                        </div>
+            `;
     }).catch(() => {
         alert(`failed to fectch weather deyails`);
     });
